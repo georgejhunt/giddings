90fa4a3a13ec1d6d0d3babab404539b3e82c0c68
George Hunt <georgejhunt@gmail.com>
2019-02-26 14:09:49 -0800

    remove pipe inpput stuff

--- fetch_wp.py
+++ fetch_wp.py
@@ -4,33 +4,25 @@
 
 import argparse
 import xml.etree.ElementTree as ET
-#from lxml import etree as ET
-import sys
+import sys,os
+import urllib2
 
 # declare namespace
 ns = { "wp": "http://wordpress.org/export/1.2/",
       "content":"http://purl.org/rss/1.0/modules/content/"}
+
 parser = argparse.ArgumentParser(description='Download wordpress media.')
-parser.add_argument('--file', dest='filename',
-                    help='Wordpress exported xml file (default: pipe input)')
+parser.add_argument( 'filename',
+                    help='Wordpress exported xml file ')
 parser.add_argument('--list', dest='list', action='store_false',
                     help='Wordpress exported xml file (default: pipe input)')
-
 args = parser.parse_args()
-try:
-   with open(sys.stdin,"r") as inf:
-      instr = read(inf)
-      try:
-	 tree = ET(instr)
-      except:
-	      print('Parse error. Quitting')
-	      sys.exit(1)
-except:
-   if args.filename:
-      tree = ET.parse(args.filename)
-   else:
-      print('No export file specified, and no pipe input. Quitting')
-      sys.exit(1)
+
+if args.filename:
+   tree = ET.parse(args.filename)
+else:
+   print('No export file specified, and no pipe input. Quitting')
+   sys.exit(1)
 if args.list:
    #print('listing selected')
    root = tree.getroot()
